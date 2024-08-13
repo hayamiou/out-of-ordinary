e5d54d74ba7c020f02ddb95e7105017ab9da14b5
Muriel Ighmouracene <muriel.ighmouracene@gmail.com>
2024-08-13 12:46:58 +0200

    Init Offers index+show

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,11 @@
 class OffersController < ApplicationController
+  before_action :set_offer, only: %i[show]
+
+  def index
+    @offers = Offer.all
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
 end

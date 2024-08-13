9f4e6f3ce13b4a89e835af6c8e45d2919e15e65b
Pierre Bermudez <bermudezpierre@gmail.com>
2024-08-13 12:56:06 +0200

    Def New + Def create OfferController

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,22 @@
 class OffersController < ApplicationController
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    if @offer.save
+      redirect_to offers_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :price, :address, :places)
+  end
 end

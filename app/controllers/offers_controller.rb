37cdbee27aa3448935e794378bbce3d2ea1c3fe1
Pierre Bermudez <bermudezpierre@gmail.com>
2024-08-14 17:01:43 +0200

    Fix Index, Navbar and link

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -13,6 +13,7 @@ class OffersController < ApplicationController
 
   def show
     @offer = Offer.find(params[:id])
+    @booking = Booking.new
     @markers = [
       {
         lat: @offer.latitude,
